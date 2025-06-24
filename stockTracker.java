d14673ee949c4650d114ae37bf7fdd54831616a4
Mugunthan234 <mugunthanmugunthan839@gmail.com>
2025-06-24 10:05:40 +0530

    delete insertUpdate1 method and change end of methods

--- stockTracker.java
+++ stockTracker.java
@@ -5,32 +5,20 @@ import java.sql.Date;
 import java.util.*;
 import java.time.LocalDate;
 
-
-
 public class stockTracker{
 public static void main(String[] args) throws Exception {
-//        readDB();
-//        SnoDB();
-//        updatebyUser();
-//        StockName();
-//stockPrice();
-//    buyPrice();
-//    buyDate();
-//    buyCount();
-    brokerName();
+//      readDB();
+//      SnoDB();
+//      updatebyUser();
+//      StockName();
+//      stockPrice();
+//      buyPrice();
+//      investedAmount();
+//      buyDate();
+//      buyCount();
+//      brokerName();
 }
- public static void insertUpdate1() throws Exception{
-        String url = "jdbc:mysql://localhost:3306/stockTracker";
-        String userName = "root";
-        String password = "root";
-        String query = "insert into stockDetails values()";
-
-        Connection conn = DriverManager.getConnection(url, userName, password);
-        Statement st = conn.createStatement();
-        int row = st.executeUpdate(query);
-        System.out.print("Rows affected "+row);
-    }
-    public static void updatebyUser() throws Exception{
+public static void updatebyUser() throws Exception{
         Scanner sc = new Scanner(System.in);
      LocalDate today = LocalDate.now();
 
@@ -70,9 +58,7 @@ public static void main(String[] args) throws Exception {
 
         System.out.print("Rows affected "+r);
 
-    }
-
-
+}
 public static void readDB() throws Exception {
     String url = "jdbc:mysql://localhost:3306/stockTracker";
     String userName = "root";
@@ -113,7 +99,8 @@ public static void SnoDB() throws SQLException {
         System.out.println("Sno: " + rs.getInt(1));
     }
 
-}public static void StockName() throws Exception {
+}
+public static void StockName() throws Exception {
         String url = "jdbc:mysql://localhost:3306/stockTracker";
         String userName = "root";
         String password = "Mugunthan@mysql";
@@ -126,10 +113,8 @@ public static void SnoDB() throws SQLException {
         while (rs.next()) {
             System.out.println("StockName: " + rs.getString(1));
         }
-
-
     }
-    public static void stockPrice() throws Exception{
+public static void stockPrice() throws Exception{
     String url = "jdbc:mysql://localhost:3306/stockTracker";
     String userName = "root";
     String password = "Mugunthan@mysql";
@@ -142,9 +127,8 @@ public static void SnoDB() throws SQLException {
     while(rs.next()){
         System.out.println("stockPrice: " + rs.getFloat(1));
     }
-    }
-
-    public static void buyPrice() throws Exception{
+}
+public static void buyPrice() throws Exception{
         String url = "jdbc:mysql://localhost:3306/stockTracker";
         String userName = "root";
         String password = "Mugunthan@mysql";
@@ -155,10 +139,10 @@ public static void SnoDB() throws SQLException {
         ResultSet rs = st.executeQuery(query);
 
         while(rs.next()){
-            System.out.println("buyPrice: " + rs.getFloat(1));
+            System.out.println("buyPrice: " + (rs.getFloat(1) + rs.getFloat(+1)));
         }
-    }
-    public static void buyDate() throws Exception{
+}
+public static void buyDate() throws Exception{
         String url = "jdbc:mysql://localhost:3306/stockTracker";
         String userName = "root";
         String password = "Mugunthan@mysql";
@@ -171,8 +155,8 @@ public static void SnoDB() throws SQLException {
         while(rs.next()){
             System.out.println("buyDate: " + rs.getDate(1));
         }
-    }
-    public static void buyCount() throws Exception{
+}
+public static void buyCount() throws Exception{
         String url = "jdbc:mysql://localhost:3306/stockTracker";
         String userName = "root";
         String password = "Mugunthan@mysql";
@@ -185,8 +169,8 @@ public static void SnoDB() throws SQLException {
         while(rs.next()){
             System.out.println("BuyCount: " + rs.getFloat(1));
         }
-    }
-    public static void brokerName() throws Exception{
+}
+public static void brokerName() throws Exception{
         String url = "jdbc:mysql://localhost:3306/stockTracker";
         String userName = "root";
         String password = "Mugunthan@mysql";
@@ -199,6 +183,25 @@ public static void SnoDB() throws SQLException {
         while(rs.next()){
             System.out.println("BrokerName:"+ rs.getString(1));
         }
-    }
-    }
+}
+public static void investedAmount() throws Exception{
+        String url = "jdbc:mysql://localhost:3306/stockTracker";
+        String user = "root";
+        String password = "Mugunthan@mysql";
+        String query = "SELECT SUM(stock_BuyPrice) AS total_StockPrice FROM stockDetails";
 
+            // Load JDBC Driver
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            // Connect to Database
+            Connection con = DriverManager.getConnection(url, user, password);
+            // Create Statement and Execute Query
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            // Process Result
+            if (rs.next()) {
+                double totalAmount = rs.getDouble("total_StockPrice");
+                System.out.println("Total Amount Invested: " + totalAmount);
+            }
+
+        }
+}
